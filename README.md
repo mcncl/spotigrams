93733beaf109ce760ed175a47e663651d3db2fed
Ben McNicholl <ben@bigboi.lan>
2021-07-25 10:09:31 +1000

    :rocket: Initial commit :tada:

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spotigrams
+Ever thought to yourself *what would be the least useful Spotify TS app?* Well, here you have it; Spotigrams.
+
+Like an artist? Enter their name in to the search field and the app will locate an artist(s) with an anagram of that artist's name in their name. What value does this bring? None at all.
---
+++ api.ts
@@ -0,0 +1,19 @@
+import SpotifyWebApi = require("spotify-web-api-node");
+
+export async function authenticate(clientId: string, clientSecret: string) {
+    const spotifyApi = new SpotifyWebApi({
+        clientId,
+        clientSecret
+    });
+    try {
+        const data = await spotifyApi.clientCredentialsGrant();
+        const accessToken = data.body["access_token"];
+        console.log("The access token is " + accessToken);
+        spotifyApi.setAccessToken(accessToken);
+        return spotifyApi;
+    } catch (err) {
+        console.log(
+            "Uh oh! Ran in to an issue when retrieving acces token. Error: ", err.message
+        );   
+    }
+}
