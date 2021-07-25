9b9d9e731a948536deca7c9efa93378088aaf634
Ben McNicholl <ben@bigboi.lan>
2021-07-25 17:03:25 +1000

    Broke search functionality out of index

--- Artist.ts
+++ Artist.ts
@@ -1,7 +1,8 @@
 import SpotifyWebApi = require("spotify-web-api-node");
 import fetch = require("node-fetch");
+import { RandomIndex } from './Helpers';
 
-export async function getArtists<T>(url: string): Promise<T> {
+async function getArtists<T>(url: string): Promise<T> {
     return fetch(url)
     .then(response => {
         if (!response.ok) {
@@ -9,4 +10,16 @@ export async function getArtists<T>(url: string): Promise<T> {
         }
         return response.json()
     })
+}
+
+export async function findArtist(query: string, spotifyApi: SpotifyWebApi): Promise<object>{
+    try {
+        const anagrams = await getArtists(`http://www.anagramica.com/best/:${query}`);
+        const chosenAnagram = await anagrams["best"][RandomIndex(anagrams["best"].length)];
+        const recommendation = await spotifyApi.searchArtists(chosenAnagram, {limit: 1});
+        return await recommendation.body.artists!.items;
+    } catch (err) {
+        console.log(`Something went wrong finding an anagram, see: ${err}`);
+        
+    }   
 }
